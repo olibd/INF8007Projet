ca081457c7e229c98be6ecab01dfd4ccd580d8cc
Olivier Brochu Dufour <olibd@hotmail.com>
2020-01-27 12:12:34 -0500

    Debut du code relier au scraper
    - Creation de la classe
    - Introduction de la librarie python html.parser

---
+++ Scraper.py
@@ -0,0 +1,11 @@
+import html.parser as h
+
+class Scraper:
+    def __init__(self):
+        pass
+
+    def extract_links(self, html: str):
+        pass
+
+    def __parse_html__(self, html: str):
+        parser = h.HTMLParser()
