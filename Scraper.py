c698ccdccd58fb35b0d79b7ff0ac8f1baac65885
Olivier Brochu Dufour <olibd@hotmail.com>
2020-02-05 16:42:18 -0500

    Ajout de l'extraction des tags <a>

--- Scraper.py
+++ Scraper.py
@@ -1,11 +1,31 @@
 import html.parser as h
 
+
 class Scraper:
     def __init__(self):
         pass
 
     def extract_links(self, html: str):
-        pass
+        parser = LinkHTMLParser()
+        parser.feed(html)
 
-    def __parse_html__(self, html: str):
-        parser = h.HTMLParser()
+class LinkHTMLParser(h.HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.switch = {
+            "a": self.__handle_anchor__
+        }
+        self.extracted_links = []
+
+    def handle_starttag(self, tag, attrs) -> str:
+        try:
+            self.extracted_links.append(self.switch[tag])
+        except:
+            print("tag {} not recognized".format(tag))
+
+    def __handle_anchor__(self, attrs):
+        for attr in attrs:
+            if (attr[0] != "href"):
+                continue
+            else:
+                return attr[1]
