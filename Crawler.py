a7945baf5f7519ead617c5df9109863e89dbc1a8
Océane <oceane.destras@polymtl.ca>
2020-02-05 17:52:57 -0500

    Udpate Crawler
    - Fonction crawl (parcours links sans doublon)
    - Fonction get_res avec bibliothèque requests retourne (link, status_code)

--- Crawler.py
+++ Crawler.py
@@ -10,15 +10,45 @@
 # Je reçois : ["HTTP://google.com","bl.com"]
 # Je retourne : [("google.com", 200), ...], objet http.response
 
-import http.client
+import requests
+import http
 
+#html = requests.get(url)
+#parsed = urlparse(url)
 
 class Crawler(object):
 
     def __init__(self, links):
         self.links = links
-        self.checked = bool
+        self.checked = {}
+        self.responses = []
 
-    def url_connection(self):
-        pass
+    def get_res(self, item):
+        #response = http.client.HTTPSConnection(self.links[item])
+        try:
+            response = requests.get(self.links[item])
+            return self.links[item], response.status_code
+        except ConnectionError:
+            return False
 
+    def get_html(self, item):
+        html = (self.links[item].get_res()).text
+        return html
+
+    def crawl(self):
+        i = 0
+        for link in self.links:
+            if link in self.checked:
+                continue
+            else:
+                self.checked[link] = i
+                res = self.get_res(i)
+                self.responses.append(res)
+            i += 1
+
+
+#links = ["www.python.org","www.google.com","www.CeLienNeMarchePas.com"]
+links = ["https://www.python.org/","https://www.google.com/","https://www.CeLienNeMarchePas.com/"]
+crawler = Crawler(links)
+crawler.crawl()
+print(crawler.responses)
