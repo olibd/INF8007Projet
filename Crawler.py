0f626981f8531e25fc3fa1463d26ee580ce455ea
Océane <oceane.destras@polymtl.ca>
2020-02-10 11:55:04 -0500

    Exception crawler
    - prise en compte de l'erreur : requests.exceptions.ConnectionError
    - Modification get_html
    A faire : définir checked_url comme variable globale ?

--- Crawler.py
+++ Crawler.py
@@ -4,35 +4,31 @@
 
 # Avec des liens de départ, les checker un par un, télécharger le contenue
 # html des liens qui ont aboutis
-
 # Fonction qui check tout les liens d'une page sans télécharger le contenue
-# library : http.client
+# library : requests
 # Je reçois : ["HTTP://google.com","bl.com"]
-# Je retourne : [("google.com", 200), ...], objet http.response
+# Je retourne : [("google.com", 200), ...]
 
 import requests
-import http
 
-#html = requests.get(url)
-#parsed = urlparse(url)
+checked_url = {}  # Définir cette liste comme une variable globale
 
-class Crawler(object):
+class Crawler:
 
-    def __init__(self, links):
-        self.links = links
-        self.checked = {}
+    def __init__(self, urls, checked):
+        self.links = urls
+        self.checked = checked
         self.responses = []
 
     def get_res(self, item):
-        #response = http.client.HTTPSConnection(self.links[item])
         try:
-            response = requests.get(self.links[item])
+            response = requests.head(self.links[item])
             return self.links[item], response.status_code
-        except ConnectionError:
-            return False
+        except requests.exceptions.ConnectionError:
+            return self.links[item], 0
 
-    def get_html(self, item):
-        html = (self.links[item].get_res()).text
+    def get_html(self, link):
+        html = requests.get(link).text
         return html
 
     def crawl(self):
@@ -48,7 +44,10 @@ class Crawler(object):
 
 
 #links = ["www.python.org","www.google.com","www.CeLienNeMarchePas.com"]
-links = ["https://www.python.org/","https://www.google.com/","https://www.CeLienNeMarchePas.com/"]
-crawler = Crawler(links)
+links = ["https://www.python.org/", "https://www.google.com/", "https://www.CeLienNeMarchePas.com/",
+         "http://www.dianping.com/promo/208721#mod=4", "https://www.google.com/"]
+crawler = Crawler(links, checked_url)
 crawler.crawl()
 print(crawler.responses)
+print(crawler.responses)
+# print(crawler.get_html("https://www.python.org/"))
