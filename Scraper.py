71a451c824c7ff1a42311faad24bccbd00596913
Olivier Brochu Dufour <olibd@hotmail.com>
2020-02-20 17:32:50 -0500

    reworked the regex to exclude more character types

--- Scraper.py
+++ Scraper.py
@@ -1,5 +1,6 @@
 import html.parser as h
 import re
+import urllib.parse
 
 
 class Scraper:
@@ -28,7 +29,7 @@ class Scraper:
         Find links not located in anchor tags
         :param html: HTML page in the form of a string
         """
-        regex = r"\b((http|https|ftp):\/\/)?(w{3}\.)?((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|[^-.][a-zA-Z0-9_.-]*[^-]\.[a-zA-Z]{1,24})(:\d*)?((\/\S*)?(\/)?)*"
+        regex = r"\b((http|https|ftp):\/\/)?(w{3}\.)?((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|[^-.=\[\]][a-zA-Z0-9_.-]*[^-=\]\[]\.[a-zA-Z]{1,24})(:\d*)?((\/\S*)?(\/)?)*"
         pattern = re.compile(regex)
         link_list = [match.group(0) for match in pattern.finditer(html)]
         return link_list
