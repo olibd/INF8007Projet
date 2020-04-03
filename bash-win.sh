7bb476da846f46a4e71044e33c7ee4ca8dcba419
Océane <oceane.destras@polymtl.ca>
2020-04-03 15:16:26 -0400

    Bash pour windows

---
+++ bash-win.sh
@@ -0,0 +1,23 @@
+echo "Usage: bash.sh [-u] git_url [-p] port. You should execute this script within the project directory."
+while getopts ":u:p:" opt; do
+  case ${opt} in
+    u )
+      url=${OPTARG}
+      ;;
+    p )
+      port=${OPTARG}
+      ;;
+    \? )
+      exit 1
+      ;;
+  esac
+done
+mkdir "localserver"
+cd ./localserver
+git clone $url .
+npm install
+PORT=$port npm start &
+echo "sleeping for 5 seconds to give enough time for the webserver to start before crawling"
+sleep 5
+cd ../
+python3 main.py url http://localhost:$port
